f1be1a060247d8317899ae55714f19226413dff2
Ar12agnik <102238499+Ar12agnik@users.noreply.github.com>
2023-05-25 00:54:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pyttsx3
 import speech_recognition as sr
 from googlesearch import search
 import webbrowser
+import pywhatkit 
 def get_responce(prompt):   
 
     token = 'WgiS6pA9JILal8aWQ4P2ObGEr_sky4LhqhbNmnHJfhG0Cynbk0YOfVj7p_gWRiWbUtBUBw.'
@@ -25,12 +26,18 @@ def hear():
         return text
     except sr.UnknownValueError:
         print("I didn't understand what you said.")
+def generate_maps_link(f,t):
+    f=f.replace(" ",'_')
+    t=t.replace(" ","_")
+    return f"https://www.google.co.in/maps/dir{f}/{t}"
 
 running=True
 speak("Hello I am your Assistant Jarvis. Press enter to start talking")
 while running:
     input("press enter to start!")
     x=hear()
+    if x is not None:
+        x=x.lower()
     if x is None:
         pass
     elif  'open' in x:
@@ -38,7 +45,22 @@ while running:
         for url in search(x):
             f_url=url
             break
-        webbrowser.open(f_url)        
+        webbrowser.open(f_url)  
+    elif 'play' in x:
+        speak("playing...")
+        pywhatkit.playonyt(x.replace("play",""))
+    elif 'send' in x:
+        pywhatkit.sendwhatmsg_instantly(input("enter the phone number with country code: "),input("what is the message ?"))
+        speak("Done! Message Sent!")
+    elif ('find') and ('phone') in x:
+        webbrowser.open_new_tab('https://www.google.com/android/find/')
+        speak("Here's a website that can help you may need to login first.")
+    elif 'directions' in x:
+        speak("where from?")
+        f=hear()
+        speak("where to?")
+        t=hear()
+        webbrowser.open_new_tab(generate_maps_link(f,t))
     else:
         count=10
         for lines in get_responce(x).split("."):
